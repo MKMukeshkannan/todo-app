43808682f61b92246ae9c9953f486143d8761b29
MKMukeshKannan <mukeshkannan311@gmail.com>
2023-04-16 21:30:59 +0530

    FIX BUGS, MADE MIGRATIONS, REGISTERED TO ADMIN
    fixed bug -> importing file from above directory. Made migrations, created super user
    registered these models in admin to work with them in 'admin' page..

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from ..user.models import User
+from user.models import User
 
 class todoDB(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
