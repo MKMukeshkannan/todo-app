ed8809ee38844ea7f264d4a7ffb6e6f28ebe249b
MKMukeshKannan <mukeshkannan311@gmail.com>
2023-04-16 19:47:58 +0530

    CREATE TWO APPS -- USER AND TODO
    created seperate app for handling user login, signup, logout logic and the todo itself
    created models for two apps and registered them in setting.py file..

---
+++ todo/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from ..user.models import User
+
+class todoDB(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    taskDesc = models.CharField(max_length=200)
+    status = models.BooleanField(default=False)
