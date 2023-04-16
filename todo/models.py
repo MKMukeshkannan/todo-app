ae784b4e0610b9e02b9508d4716f1e731e0a33e5
MKMukeshKannan <mukeshkannan311@gmail.com>
2023-04-16 21:49:11 +0530

    ADD __str__ DUNDER FUNCTIONS TO MODEL
    add some sample data to work with, add dunder method to display its name.

--- todo/models.py
+++ todo/models.py
@@ -5,3 +5,7 @@ class todoDB(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     taskDesc = models.CharField(max_length=200)
     status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.taskDesc[:7] + self.user.first_name
+    
