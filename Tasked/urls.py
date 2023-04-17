e0e2de2d70a8240a1fe2c747a1fbad69b66ef485
MKMukeshKannan <mukeshkannan311@gmail.com>
2023-04-17 21:00:43 +0530

    ADD USER CREATION LOGIN AND LOGOUT LOGIC
    completly functional user creation function that creates new users in our custom form.
    created a custom user creation from in forms.py file.Added User Logout functionality..

--- Tasked/urls.py
+++ Tasked/urls.py
@@ -4,6 +4,7 @@ from todo.views import home as todoHome
 from todo.views import todoMain as todoList
 from todo.views import todoDelete, todoDone
 from django.contrib.auth import views as authViews
+from user.views import customUserCreation
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,6 +12,7 @@ urlpatterns = [
     path('todo/',todoList, name = 'todo'),
     path('<int:pk>/delete', todoDelete, name = 'todoDelete'),
     path('<int:pk>/done', todoDone, name = 'todoDone'),
-    path('login/',authViews.LoginView.as_view(template_name = 'user/login.html'), name = 'login')
-
+    path('login/',authViews.LoginView.as_view(template_name = 'user/login.html'), name = 'login'),
+    path('logout/',authViews.LogoutView.as_view(template_name = 'todo/index.html'), name = 'logout'),
+    path('signup/', customUserCreation.as_view(), name = 'signup')
 ]
