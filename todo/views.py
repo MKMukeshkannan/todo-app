3c5863f292f3dea6f10a2167aef7c1297ef19a91
MKMukeshKannan <mukeshkannan311@gmail.com>
2023-04-17 11:27:53 +0530

    ADD TASK FEILD TO ENTER TASK
    Task form that can add data into the database and sucessfully display it

--- todo/views.py
+++ todo/views.py
@@ -1,15 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import todoDB
+from user.models import User
 from .forms import todoForm
 
 def home(request):
     return render(request, 'todo/index.html')
 
 def todoMain(request):
+    if request.method == 'POST':
+        form = todoForm(request.POST)
+        if form.is_valid():
+            user = User.objects.all().first()
+            todoDB.objects.create(user = user, taskDesc = form.cleaned_data['Task'])
+            return redirect('todo')
+    else:
+        form = todoForm()
+    
     data = todoDB.objects.all()
     context = {
         'data':data,
-        'form':todoForm,
+        'form':form,
     }    
     return render(request, 'todo/todo.html', context=context)
 
