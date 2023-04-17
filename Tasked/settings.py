9655eed741ffb43eb7b5e364a0d4375d50da9f28
MKMukeshKannan <mukeshkannan311@gmail.com>
2023-04-17 19:01:00 +0530

    ADDS A USER LOGIN FUNCTION

--- Tasked/settings.py
+++ Tasked/settings.py
@@ -126,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'user.User'
+
+LOGIN_REDIRECT_URL = 'todo'
--- Tasked/urls.py
+++ Tasked/urls.py
@@ -3,12 +3,14 @@ from django.urls import path
 from todo.views import home as todoHome
 from todo.views import todoMain as todoList
 from todo.views import todoDelete, todoDone
+from django.contrib.auth import views as authViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', todoHome, name = 'home'),
     path('todo/',todoList, name = 'todo'),
     path('<int:pk>/delete', todoDelete, name = 'todoDelete'),
-    path('<int:pk>/done', todoDone, name = 'todoDone')
+    path('<int:pk>/done', todoDone, name = 'todoDone'),
+    path('login/',authViews.LoginView.as_view(template_name = 'user/login.html'), name = 'login')
 
 ]
